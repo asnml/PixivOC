52a4e56450f77accba1d87e1037351a7cc3ae9ca
snfdess <AsunaML@outlook.com>
2020-03-14 11:25:36 +0800

    修复当第一次Start任务因网络原因请求Token失败时，接下来就无法启动任何需要Token的任务的bug

--- PixivOC/mobileToken.py
+++ PixivOC/mobileToken.py
@@ -157,6 +157,7 @@ class TokenManager:
             return self._extract_token(result.result.decode())
         else:
             core_logger.warning('Request refresh token failed, please check internal connection.')
+            self._Refreshing = False
             while self._SendTokenList:
                 callback = self._SendTokenList.pop()
                 callback(0)
